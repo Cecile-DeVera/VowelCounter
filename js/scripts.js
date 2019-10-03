1862782474a97bb8a90ea18accf4260efee54a33
Andrew Philpott and Cecile DeVera <cecile@>
2019-10-02 19:17:55 -0700

    Add index.html, styles.css,scripts.js, bootstrap, jquery, readme.md. Add user form and initial javascript logic to get user input.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  var vowelCountCalculated;
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var userInputSentence = $("#userInput").val();
+
+  });
+  function countVowels(sentence) {
+    var letters = sentence.split("");
+    if () {
+
+    }
+  }
+});
