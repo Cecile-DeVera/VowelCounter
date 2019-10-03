50a9676ec6a3c448d378c76969c26fc3d4d16445
Andrew Philpott and Cecile DeVera <cecile@>
2019-10-02 20:02:02 -0700

    Add logic for vowel counting a sentence containing vowels, non vowels, numbers, and special characters.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-  var vowelCountCalculated;
   $("form").submit(function(event) {
     event.preventDefault();
+    $("#result").show();
     var userInputSentence = $("#userInput").val();
-
+    $("#result p").text(countVowels(userInputSentence).toString());
   });
   function countVowels(sentence) {
+    var vowelCountCalculated = 0;
     var letters = sentence.split("");
-    if () {
-
-    }
+    for (var i = 0; i < letters.length; i++) {
+      var letterLowercase = letters[i].toLowerCase();
+      if (Number.isInteger(parseInt(letterLowercase))) {
+        vowelCountCalculated += 0;
+      } else if (letterLowercase === "a" || letterLowercase === "e" || letterLowercase === "o" || letterLowercase === "e") {
+        vowelCountCalculated += 1;
+      } else {
+        vowelCountCalculated += 0;
+      }
   }
+    return vowelCountCalculated;
+}
 });
